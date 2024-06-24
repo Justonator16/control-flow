196bb9d8d28a3a2f790670ba462574ddfbfff4ed
Justonator16 <131472241+Justonator16@users.noreply.github.com>
2024-06-24 12:39:15 +0000

    pattern program

--- pattern_drawing.py
+++ pattern_drawing.py
@@ -1,5 +1,4 @@
-integer = input("Enter the size of the pattern: ")
+integer = int(input("Enter the size of the pattern: "))
 
 for i in range(integer):
-    for j in range(integer):
-        print(j)
+    print("*"*integer)
